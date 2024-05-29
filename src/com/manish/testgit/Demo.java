66a7cafae565a02ebc658a32c8ecfa11685ff77e
manishmdeshpande <manishmdeshpande17@gmail.com>
2024-05-29 20:32:38 +0530

    First commit

---
+++ src/com/manish/testgit/Demo.java
@@ -0,0 +1,10 @@
+package com.manish.testgit;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is for testing first git command push");
+	}
+
+}
