edb75c0bdad591a182d5a92b10da73e2cf77adca
manishmdeshpande <manishmdeshpande17@gmail.com>
2024-05-29 20:36:56 +0530

    second commit

--- src/com/manish/testgit/Demo.java
+++ src/com/manish/testgit/Demo.java
@@ -5,6 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is for testing first git command push");
+		System.out.println("This is 2nd commit to git");
+
 	}
 
 }
